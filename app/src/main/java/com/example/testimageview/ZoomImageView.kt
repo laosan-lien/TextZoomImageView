da70742e0256f106ced8d69afbfa618a3d98255a
= <=>
2022-02-16 02:04:57 +0800

    图片居中显示

--- app/src/main/java/com/example/testimageview/ZoomImageView.kt
+++ app/src/main/java/com/example/testimageview/ZoomImageView.kt
@@ -2,55 +2,90 @@ package com.example.testimageview
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.graphics.PointF
 import android.opengl.Matrix
-import android.util.AttributeSet
-import android.widget.ImageView
-import android.graphics.drawable.Drawable
-import android.view.View.MeasureSpec
 
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.ImageView
+
+
+private const val TAG = "NativeUi:ZoomImageView"
 
 class ZoomImageView : androidx.appcompat.widget.AppCompatImageView {
-    constructor(context: Context) : super(context){
+
+    constructor(context: Context) : super(context) {
         init()
     }
-    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet){
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         init()
     }
+
     constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(
         context,
         attributeSet,
         defStyleAttr
-    ){
+    ) {
         init()
     }
 
-    //    private lateinit var  matrix:Matrix
     private lateinit var viewSize: PointF
     private lateinit var imageSize: PointF
 
+    //缩放后图片的大小
+    private var scaleSize = PointF()
+
+    //最初的宽高的缩放比例
+//    private lateinit var originScale :PointF
+
+
     private fun init() {
-        scaleType = ScaleType.MATRIX
+//        scaleType = ScaleType.MATRIX
     }
 
 
+    //TODO：更改
+    @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(TAG, "onMeasure")
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val width = MeasureSpec.getSize(widthMeasureSpec)
         val height = MeasureSpec.getSize(heightMeasureSpec)
         viewSize = PointF(width.toFloat(), height.toFloat())
+        imageSize = PointF(width.toFloat(), width.toFloat())
 
-        val drawable = drawable
-        imageSize = PointF(
-            drawable.minimumWidth.toFloat(),
-            drawable.minimumHeight.toFloat()
-        )
+        showCenter()
     }
-//
-//    private fun showCenter(){
-//        val scaleX = viewSize.x / imageSize.x
-//        val scaleY =
-//    }
+
+    private fun showCenter() {
+        Log.d(TAG, "showCenter")
+        val scaleX = viewSize.x / imageSize.x
+        val scaleY = viewSize.y / imageSize.y
+        val scale = if (scaleX < scaleY) scaleX else scaleY
+        scaleImage(PointF(scale, scale))
+    }
+
+    /**
+     * 将图片按照比例缩放，这里宽高缩放比例相等，所以PointF 里面的x,y是一样的
+     * @param scaleXY
+     */
+    private fun scaleImage(scaleXY: PointF) {
+        Log.d(TAG, "scaleImage: ")
+        matrix.setScale(0.1f, 0.1f)
+        matrix.setRotate(30f)
+        scaleSize.set(scaleXY.x * imageSize.x, scaleXY.y * imageSize.y)
+        imageMatrix = matrix
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.save()
+        canvas?.drawBitmap(BitmapFactory.decodeResource(resources, R.drawable.img), matrix , null)
+    }
 
 
 
